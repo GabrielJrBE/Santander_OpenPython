82cb9146990dcec46485bc6b7a563cfa8aca51bb
Gabriel Junior <Gabrieljr.BE@gmail.com>
2025-05-26 00:06:28 +0200

    Tuplas

---
+++ Santander_OpenPython/Aulas/1.0 Introduçao/4.0Estruturas de dados/Lista_Criação_e_acesso.py
@@ -0,0 +1,10 @@
+frutas = ["maça","banana","laranja"]
+
+print(frutas[0])
+print(frutas[1])
+print(frutas[2])
+
+# Você também pode acessar os elementos a partir do final da lista utilizando índices negativos.
+print(frutas[-1])
+print(frutas[-2])
+print(frutas[-3])
